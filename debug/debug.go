621d1dd086f790b5db402470bd036b2bdd088861
Thomas Fischer <tdf.tomfischer@gmail.com>
2016-04-16 21:07:31 -0500

    Add entity and player registration.

--- debug/debug.go
+++ debug/debug.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/gragas/go-sdl2/sdl"	
 	"github.com/gragas/jabberwock-client/utils"
+	"github.com/gragas/jabberwock-lib/entity"
+	"github.com/gragas/jabberwock-lib/player"
+	"github.com/gragas/jabberwock-lib/protocol"
 	"github.com/gragas/jabberwock-server/game"
 	"net"
 )
@@ -16,8 +19,24 @@ func Init(ip string, port int, quiet bool, debug bool, serverDebug bool) {
 	<-done
 	var registered bool
 	var attempts int
+	player := player.Player{Entity: entity.Entity{ID: uint64(protocol.GenerateEntityID),
+		Name: player.DefaultName,
+		Health: player.DefaultHealth,
+		MaxHealth: player.DefaultMaxHealth,
+		Energy: player.DefaultEnergy,
+		MaxEnergy: player.DefaultMaxEnergy,
+		Spirit: player.DefaultSpirit,
+		MaxSpirit: player.DefaultMaxSpirit,
+		Summoning: player.DefaultSummoning,
+		Alteration: player.DefaultAlteration,
+		Willpower: player.DefaultWillpower,
+		Divinity: player.DefaultDivinity,
+		Lifebringing: player.DefaultLifebringing,
+		X: player.DefaultX, Y: player.DefaultY,
+		Xv: player.DefaultXv, Yv: player.DefaultYv,
+		W: player.DefaultW, H: player.DefaultH}}
 	for ; attempts < 5; attempts++ {
-		conn, registered = utils.RegisterClient(ip, port, debug)
+		conn, registered = utils.RegisterClient(ip, port, player, debug)
 		if registered {
 			break
 		}
--- utils/utils.go
+++ utils/utils.go
@@ -3,7 +3,8 @@ package utils
 import (
 	"bufio"
 	"fmt"
-	"github.com/gragas/go-sdl2/sdl"	
+	"github.com/gragas/go-sdl2/sdl"
+	"github.com/gragas/jabberwock-lib/player"
 	"github.com/gragas/jabberwock-lib/protocol"
 	"net"
 	"strconv"
@@ -17,36 +18,45 @@ var Loop LoopFuncs
 
 type LoopFuncs struct {
 	PollEvents func()
-	Update func()
-	Draw func()
+	Update     func()
+	Draw       func()
 }
 
-func RegisterClient(ip string, port int, debug bool) (net.Conn, bool) {
+func RegisterClient(ip string, port int, player player.Player,
+	debug bool) (net.Conn, bool) {
 	if debug {
 		fmt.Printf("CLIENT: Registering with server at \033[0;31m")
 		fmt.Printf("%v\033[0m:\033[0;34m%v\033[0m\n", ip, port)
 	}
-	
-	registrationFailure := func(address string) (net.Conn, bool) {
+
+	registrationFailure := func(address string, malformed bool) (net.Conn, bool) {
 		fmt.Printf("CLIENT: Could not register with server at %s!\n", address)
+		if malformed {
+			fmt.Printf("CLIENT: Additional info: received malformed response.\n")
+		}
 		return nil, false
 	}
 
 	address := ip + ":" + strconv.Itoa(port)
 	conn, err := net.Dial("tcp", address)
 	if err != nil {
-		return registrationFailure(address)
+		return registrationFailure(address, false)
 	}
-	fmt.Fprintf(conn, string(protocol.Register) + "\n")
-	status, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil || protocol.Code(status[0]) != protocol.Success || len(status) < 2 {
-		return registrationFailure(address)
+	sendstr := string(protocol.Register)
+	sendstr += player.String() + string(protocol.EndOfMessage)
+	fmt.Fprintf(conn, sendstr)
+	status, err := bufio.NewReader(conn).ReadString(byte(protocol.EndOfMessage))
+	if err != nil || protocol.Code(status[0]) != protocol.Success {
+		return registrationFailure(address, false)
+	}
+	if len(status) < 2 {
+		return registrationFailure(address, true)
 	}
 	if debug {
 		fmt.Printf("CLIENT: Successfully registered client with server at %s\n",
 			address)
 		fmt.Printf("CLIENT: Response from server was '%v'\n",
-			protocol.Code(status[0]).String() + status[1:len(status)-1])
+			protocol.Code(status[0]).String()+status[1:len(status)-1])
 	}
 	return conn, true
 }
